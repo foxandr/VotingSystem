a445d38aae2c88f357da6ae06f82c2f509c00b8e
foxandr <ako.fox@gmail.com>
2016-08-17 18:23:47 +0300

    model and repository objects

---
+++ src/main/java/fox/alex/votingsystem/repository/datajpa/ProxyUserRepository.java
@@ -0,0 +1,36 @@
+package fox.alex.votingsystem.repository.datajpa;
+
+import fox.alex.votingsystem.model.User;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * Created by fox on 17.08.16.
+ */
+@Transactional(readOnly = true)
+public interface ProxyUserRepository extends JpaRepository<User, Integer> {
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM User u WHERE u.id = :id")
+    int delete(@Param("id") int id);
+
+    @Override
+    @Query("SELECT DISTINCT u FROM User u LEFT JOIN FETCH u.roles, u.votes ORDER BY u.name, u.email")
+    List<User> findAll();
+
+    @Override
+    @Transactional
+    User save(User user);
+
+    @Override
+    @EntityGraph(value = User.GRAPH_WITH_ROLES)
+    User findOne(Integer integer);
+
+}
